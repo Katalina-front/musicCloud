0d8f882a1514025c0b90c3db72bbc8ba53f7d17c
e.hozikova <e.hozikova@univertv.ru>
2025-02-04 14:38:55 +0400

    MW-3: 04.02.25

--- src/store/reducers/track/track-reducer.ts
+++ src/store/reducers/track/track-reducer.ts
@@ -5,6 +5,20 @@ const initialState: TrackState = {
     errore: ''
 }
 
-export const trackReducer = (state, actiion) => {
-
+export const trackReducer = (state: any, actiion: { type: any; payload: any }) => {
+    switch (actiion.type) {
+        case 'FETCH_TRACKS':
+            return {
+                ...state,
+                tracks: actiion.payload
+            }
+        case 'FETCH_TRACKS_ERROR':
+            return {
+                ...state,
+                errore: actiion.payload
+            }
+        default:
+            return state
+    }
 }
+ 
